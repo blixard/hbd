3a19a38e29291a2f52ebc7433cb701437d429cf0
Shubha <rupelpadhy@gmail.com>
2021-05-04 21:48:51 +0530

    bullet of sand

--- script/script.js
+++ script/script.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    alert("working");
+    animate();
+    console.log("working")
 })
+
+animate = ()=>{
+    var tl = gsap.timeline(
+        {
+            scrollTrigger:{
+                duration:2,
+                trigger:".intro",
+                markers:true,
+                start:"top mid",
+                end:"+=10000",
+                scrub:true
+            }
+        }
+    )
+
+    tl.from(".intro",{
+        opacity:0
+    })
+    tl.from(".introt",{
+        opacity:0
+    })
+    tl.from(".mid",{
+        opacity:0
+    })
+    tl.from(".midt",{
+        opacity:0
+    })
+    tl.from(".end",{
+        opacity:0,
+    })
+
+
+    tl.from(".endt",{
+        opacity:0,
+    })
+}
